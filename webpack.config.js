0eaead0fe47f2ec8f5aaa5faee1e10e1d8422fef
Nick Arcuri <narcuri1@gmail.com>
2016-04-24 12:27:08 -0400

    Setup project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: __dirname + '/App/App.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public',
+    publicPath: 'http://localhost:8080/public/'
+  },
+  devTool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: [ 'es2015', 'react' ]
+        }
+      },
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  }
+};
